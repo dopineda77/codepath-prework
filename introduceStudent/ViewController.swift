87647757d9ada3556c50e26f8e32500f5b49a6c2
dopineda77 <dopineda77@gmail.com>
2023-01-01 17:39:31 -0800

    Initial Commit

---
+++ introduceStudent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  introduceStudent
+//
+//  Created by Danny Pineda on 1/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
